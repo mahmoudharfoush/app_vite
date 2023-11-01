aa719254242213452eda4022bbbe81fe51b18069
mahmoudharfoush <mahmoudharfoush5320@gmail.com>
2023-11-01 14:47:53 +0200

    start vite project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar.jsx'
+import Footer from './components/footer/Footer.jsx'
+import Products from './components/products/Products.jsx'
+import Home from './components/home/Home.jsx'
+
+export default function App() {
+  return (
+    <>
+    <Navbar />
+    <Products />
+    <Home />
+    <Footer />
+    </>
+
+  )
+}
